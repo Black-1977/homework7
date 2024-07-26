a7a7bb3bdb2bb2934a799024fb2a5e2f77793f70
Black77 <islepuchin@yandex.ru>
2024-07-26 23:54:52 +0300

    work done

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ public class Main {
         System.out.println("Задание 1");
         int monthNumber = 0;
         int contribution = 15_000;
+        int percent = 12;
         int total = 0;
         while (total <= 2_459_000) {
             monthNumber++;
-            total += contribution;
+            total += total * percent / 100 + contribution;
             System.out.println("Месяц " + monthNumber + ", сумма накоплений равна " + total + " рублей");
         }
         System.out.println("Потребуется " + monthNumber + " месяцев");
@@ -42,9 +43,10 @@ public class Main {
         System.out.println("Задание 4");
         total = 15_000;
         monthNumber = 0;
+        percent = 7;
         while (total < 12_000_000) {
             monthNumber++;
-            total += (int) (total * 0.07);
+            total += total * percent / 100;
             System.out.println("Месяц " + monthNumber + ", сумма накоплений " + total);
         }
         System.out.println("Потребуется " + monthNumber + " месяцев");
@@ -54,9 +56,10 @@ public class Main {
         System.out.println("Задание 5");
         total = 15_000;
         monthNumber = 0;
+        percent = 7;
         while (total < 12_000_000) {
             monthNumber++;
-            total += (int) (total * 0.07);
+            total += total * percent / 100;
             if (monthNumber % 6 == 0) {
                 System.out.println("Месяц " + monthNumber + ", сумма накоплений " + total);
             }
@@ -67,11 +70,12 @@ public class Main {
         // Par 6
         System.out.println("Задание 6");
         total = 15_000;
+        percent = 7;
         int totalMonth = 9 * 12;
         monthNumber = 0;
         do {
             monthNumber++;
-            total += (int) (total * 0.07);
+            total += total * percent / 100;
             if (monthNumber % 6 == 0) {
                 System.out.println("Месяц " + monthNumber + ", сумма накоплений " + total);
             }
@@ -91,9 +95,10 @@ public class Main {
         // Par 8
         System.out.println("Задание 8");
         int currentYear = 2024;
+        int cometPeriod = 79;
         int year = currentYear - 200;
         while (year <= currentYear + 100) {
-            if (year % 79 == 0) {
+            if (year % cometPeriod == 0) {
                 System.out.println(year);
             }
             year++;
